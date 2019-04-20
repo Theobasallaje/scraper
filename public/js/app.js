7f897b13b739d5c9926fcc2ff4175aa92eca4d35
DESKTOP-GOA90O0\Theo Basallaje <theobasallaje@gmail.com>
2019-04-20 00:59:24 -0500

    trying to add a user property to results onject

--- public/js/app.js
+++ public/js/app.js
@@ -3,8 +3,9 @@ $.getJSON("/articles", function(data) {
   // For each one
   for (var i = 0; i < data.length; i++) {
     // Display the apropos information on the page
-    $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
+    $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "<br />" + data[i].user + "</p>");
   }
+  console.log(data[0]);
 });
 
 
