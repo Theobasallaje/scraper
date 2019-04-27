99268dcf964c103344006cd1c9d28ea80b528b36
Theo <theobasallaje@gmail.com>
2019-04-27 00:36:41 -0500

    ugly complete v1

--- public/js/app.js
+++ public/js/app.js
@@ -1,18 +1,22 @@
 // Grab the articles as a json
-$.getJSON("/articles", function(data) {
+$.getJSON("/articles", function (data) {
   // For each one
   for (var i = 0; i < data.length; i++) {
     // Display the apropos information on the page
     $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "<br />" + data[i].user + "</p>");
   }
-  console.log(data[0]);
+  // console.log(data[0]);
 });
+
+//notes id is in data.note._id -- data and notes are arrays
 
 
 // Whenever someone clicks a p tag
-$(document).on("click", "p", function() {
+$(document).on("click", "p", function () {
   // Empty the notes from the note section
   $("#notes").empty();
+  $("#header").empty();
+  $("#comments").empty();
   // Save the id from the p tag
   var thisId = $(this).attr("data-id");
 
@@ -22,52 +26,78 @@ $(document).on("click", "p", function() {
     url: "/articles/" + thisId
   })
     // With that done, add the note information to the page
-    .then(function(data) {
+    .then(function (data) {
       console.log(data);
-      // The title of the article
-      $("#notes").append("<h2>" + data.title + "</h2>");
-      // An input to enter a new title
-      $("#notes").append("<input id='titleinput' name='title' >");
+      // // The title of the article
+      // $("#notes").append("<h2>" + data.title + "</h2>");
+      // commenmts section
+      $("#header").append(`<h2>Comments for ${data.title}</h2>`);
+      // // An input to enter a new title
+      // $("#notes").append("<input id='titleinput' name='title' >");
+      if (data.note) {
+        for (var i = 0; i < data.note.length; i++) {
+          $("#comments").append(`<div data-id="${data.note[i]._id}" article-id="${thisId}" class="commentData" id="comment${data.note[i]._id}"><p>${data.note[i].body}</p> 
+                                <button data-id="${data.note[i]._id}" article-id="${thisId}" id='deletenote'>Delete
+                                </button></div>`); // adding delete button here
+        }
+      }
       // A textarea to add a new note body
       $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
       // A button to submit a new note, with the id of the article saved to it
-      $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
-
+      $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Add Comment</button>");
+      // $("#notes").append("<button data-id='" + data._id + "' id='deletenote'>Delete Comment</button>");
       // If there's a note in the article
-      if (data.note) {
-        // Place the title of the note in the title input
-        $("#titleinput").val(data.note.title);
-        // Place the body of the note in the body textarea
-        $("#bodyinput").val(data.note.body);
-      }
     });
 });
 
 // When you click the savenote button
-$(document).on("click", "#savenote", function() {
+var i = 0;
+$(document).on("click", "#savenote", function () {
   // Grab the id associated with the article from the submit button
   var thisId = $(this).attr("data-id");
-
+  // $("#comments").append(`<p>${$("#bodyinput").val()}</p>`);
   // Run a POST request to change the note, using what's entered in the inputs
   $.ajax({
     method: "POST",
     url: "/articles/" + thisId,
     data: {
-      // Value taken from title input
-      title: $("#titleinput").val(),
+      // // Value taken from title input
+      // title: $("#titleinput").val(),
       // Value taken from note textarea
       body: $("#bodyinput").val()
     }
   })
     // With that done
-    .then(function(data) {
+    .then(function (data) {
+      $("#comments").append(`<div data-id="${data.note[i]._id}" article-id="${thisId}" class="commentData" id="comment${data.note[i]._id}"><p>${$("#bodyinput").val()}</p> 
+      <button data-id="${data.note[i]._id}" article-id="${thisId}" id='deletenote'>Delete
+      </button></div>`);
       // Log the response
+      // var data = JSON.stringify(data);
       console.log(data);
+      // for (var i=0; i < data.note.length; i++) {
+      //   console.log(`data for index ${i} ${$("#bodyinput").val()}`);
+      //   // console.log(`data for index ${i} ${(data.note[i].body)}`);
+      // }
       // Empty the notes section
-      $("#notes").empty();
+      // $("#notes").empty();
     });
 
   // Also, remove the values entered in the input and textarea for note entry
-  $("#titleinput").val("");
-  $("#bodyinput").val("");
+  // $("#titleinput").val("");
+  // $("#bodyinput").val("");
+  i++;
+});
+
+$(document).on("click", "#deletenote", function () {
+  var commentId = $(this).attr("data-id");
+  // var deleteId = $(this).attr("delete-id");
+  var articleId = $(this).attr("article-id");
+  $.ajax({
+    type: "PUT",
+    url: "/comments/" + articleId + "/note/" + commentId
+  })
+    .then(function (data) {
+      $(`#comment${commentId}`).empty();
+    });
 });
--- server.js
+++ server.js
@@ -49,17 +49,9 @@ app.get("/scrape", function (req, res) {
 
     // Now, we grab every h2 within an article tag, and do the following:
     // Save an empty result object
-    var result = {};
-    // $("article p username").each(function (i, element) {
-    //   user.user = $(this)
-    //     .children("a")
-    //     .text();
-
-    //   // console.log(result.user);
-    // });
-
     $("article").each(function (i, element) {
-      // Add the text and href of every link, and save them as properties of the result object
+      var result = {};
+      // Add the text, href, and user of every article, and save them as properties of the result object
       result.title = $(this)
         .children("h2")
         .children("a")
@@ -83,19 +75,6 @@ app.get("/scrape", function (req, res) {
           console.log(err);
         });
     });
-
-    // for (var i = 0; i < result.length; i++){
-    //   // Create a new Article using the `result` object built from scraping
-    //   db.Article.create(result)
-    //     .then(function (dbArticle) {
-    //       // View the added result in the console
-    //       console.log(dbArticle);
-    //     })
-    //     .catch(function (err) {
-    //       // If an error occurred, log it
-    //       console.log(err);
-    //     });
-    // }
 
     // Send a message to the client
     // res.send("Scrape Complete");
@@ -133,7 +112,7 @@ app.get("/articles/:id", function (req, res) {
     });
 });
 
-// Route for saving/updating an Article's associated Note
+// Route for saving an Article's associated Note
 app.post("/articles/:id", function (req, res) {
   // Create a new note and pass the req.body to the entry
   db.Note.create(req.body)
@@ -141,7 +120,7 @@ app.post("/articles/:id", function (req, res) {
       // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
       // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
       // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
-      return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+      return db.Article.findOneAndUpdate({ _id: req.params.id }, { $push: { note: dbNote._id } }, { new: true });
     })
     .then(function (dbArticle) {
       // If we were able to successfully update an Article, send it back to the client
@@ -151,6 +130,11 @@ app.post("/articles/:id", function (req, res) {
       // If an error occurred, send it to the client
       res.json(err);
     });
+});
+
+app.put("/comments/:articleid/note/:noteid", function (req, res) {
+  console.log(req.params.id);
+  db.Article.update({ _id: req.params.articleid }, { $pull: { note: req.params.noteid } }).then(function(dbArticle){res.json(dbArticle);});
 });
 
 // Start the server
